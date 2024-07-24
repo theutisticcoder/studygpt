837307c537cddc3a56898de04284d6e83d352d45
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-07-24 11:07:39 -0400

    fix key

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const {
     HarmBlockThreshold,
   } = require("@google/generative-ai");
   
-  const apiKey = "AIzaSyB-qX79cjwUfTXMM3byFmN_3-tfetp4sDI";
+  const apiKey = process.env.api;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
